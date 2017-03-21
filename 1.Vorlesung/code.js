afbf39f7f9f0be44eba593752fbe545feac353fe
RaoulLaptop <raoul.bickmann@gmail.com>
2017-03-21 14:57:16 +0100

    first half 1. vorlesung

---
+++ 1.Vorlesung/code.js
@@ -0,0 +1,7 @@
+function initialize(){
+	let label = document.getElementById("Label");
+	label.innerHTML = "Hello";
+	label.style['color'] = 'red';
+	label.style['background-color'] = 'blue';
+	label.style['font-size'] = '96px';
+}
