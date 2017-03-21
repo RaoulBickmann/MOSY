103bdd45542c2a2c8ea7eb344b999189ea737f60
RaoulLaptop <raoul.bickmann@gmail.com>
2017-03-21 21:59:08 +0100

    zweiter teil

---
+++ 1.Vorlesung/canvas/code.js
@@ -0,0 +1,58 @@
+function initialize(){
+	const canvas = document.getElementById("graphics");
+	const ctx = canvas.getContext("2d");
+	
+	ctx.fillStyle = '#FFFF00';
+	ctx.fillRect(50, 100, 380, 400);
+	ctx.fillStyle = "rgba(0, 0, 124, 0.8";
+	ctx.fillRect(25, 50, 70, 190);
+
+	class Square{
+		constructor(x, y, width) {
+			this.x = x;
+			this.y = y;
+			this.width = width;
+		}
+		draw(context){
+			context.fillRect(this.x, this.y, this.width, this.width);
+		}
+	}
+
+	let square = new Square(10, 10 , 30);
+	square.draw(ctx);
+
+	class ColoredSquare extends Square{
+		constructor(x, y, width, color) {
+			super(x, y, width);
+			this.fillStyle = color;
+		}
+		draw(context) {
+			context.fillStyle = this.fillStyle;
+			super.draw(context);
+		}
+	}
+	let coloredSquare = new ColoredSquare(10, 20, 50, "rgba(0, 255, 122, 0.4)");
+	coloredSquare.draw(ctx);
+
+	// mousemove EventListener
+	function onMouseMove(e) {
+		const x = e.pageX;
+		const y = e.pageY;
+		coloredSquare.fillStyle = "#FFFFFF";
+		coloredSquare.draw(ctx)
+		coloredSquare.x = x-35;
+		coloredSquare.y = y-35;
+		coloredSquare.fillStyle = "rgba(0, 255, 122, 0.4)";
+		coloredSquare.draw(ctx);
+	}
+	const container = document.getElementById("container");
+	container.addEventListener("mousemove", onMouseMove);
+
+}	
+
+
+
+
+
+
+
--- 1.Vorlesung/code.js
+++ 1.Vorlesung/code.js
@@ -4,4 +4,19 @@ function initialize(){
 	label.style['color'] = 'red';
 	label.style['background-color'] = 'blue';
 	label.style['font-size'] = '96px';
+
+	//Slider Eventhandler
+	const sliderRed = document.getElementById("sliderRed");
+	const sliderBlue = document.getElementById("sliderBlue");
+	const sliderGreen = document.getElementById("sliderGreen");
+	function onSliderChanged() {
+		let body = document.getElementById("body");
+		body.style['background-color'] = "rgb(" + sliderRed.value + "," + sliderBlue.value + "," + sliderGreen.value + ")";
+	}
+	
+	sliderRed.addEventListener("input", onSliderChanged);
+	sliderBlue.addEventListener("input", onSliderChanged);
+	sliderGreen.addEventListener("input", onSliderChanged);
+
+	onSliderChanged();
 }
